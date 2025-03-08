61166f7bf8b4291109e78eb97933edb637f1da72
Shalini Chaturvedy <shalinichaturvedy@gmail.com>
2025-03-08 10:00:08 +0530

    Demo changes

--- app.py
+++ app.py
@@ -53,4 +53,6 @@ def predict_result ():
 
 if __name__ == '__main__':
     # app.run (debug = False, host='0.0.0.0', port=5000) 
-    app.run(debug = False)
+    # app.run(debug = False)
+    app.run(debug=False, host='0.0.0.0', port=5001)
+
